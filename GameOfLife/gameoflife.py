46f3a959496512d2cb5a176baa245f7882608a93
Patrick Hanckmann <patrick.hanckmann@tno.nl>
2016-09-10 09:07:01 +0200

    rewrite to functional code, more flexible, worx

--- GameOfLife/gameoflife.py
+++ GameOfLife/gameoflife.py
@@ -5,75 +5,60 @@ e-mail: patrick@hanckmann.com
 """
 
 import numpy
-import time
 from random import uniform
 
 
-class GameOfLife(object):
-    """ Basic game-of-world implementation """
+def random_world(width=10, height=10, fill_chance=.15):
+    """ Generate random world """
+    world = numpy.zeros((width, height))
+    # Generate random world
+    for wi in range(0, width):
+        for hi in range(0, height):
+            rvalue = uniform(0.0, 1.0)
+            if rvalue <= fill_chance:
+                world[wi, hi] = 1
+    return world
 
-    def __init__(self, world_width=10, world_height=10, fill_chance=.15):
-        """ Init """
-        self.world_width = world_width
-        self.world_height = world_height
-        self.world = numpy.zeros((world_width, world_height))
-        # Set start world (at random)
-        for wi in range(0, world_width):
-            for hi in range(0, world_height):
-                rvalue = uniform(0.0, 1.0)
-                if rvalue <= fill_chance:
-                    self.world[wi, hi] = 1
 
-    def run(self, steps=100, wait_time=1.0):
-        """ Start time """
-        next_world = self.world
-        self.show_world(next_world)
-        for _ in range(0, steps):
-            time.sleep(wait_time)
-            next_world = self.next_tick(next_world)
-            self.show_world(next_world)
-        print("finished")
+def next_tick(world, born=[3], survive=[2, 3]):
+    """ Execute gol rules on world (default rules: B3,S23) """
+    (width, height) = world.shape
+    next_world = numpy.zeros((width, height))
+    for wi in range(0, width):
+        for hi in range(0, height):
+            wi_plus = wi + 1
+            hi_plus = hi + 1
+            if wi == width - 1:
+                wi_plus = -1
+            if hi == height - 1:
+                hi_plus = -1
+            neighbours = 0
+            neighbours += world[wi - 1, hi - 1]
+            neighbours += world[wi - 1, hi]
+            neighbours += world[wi - 1, hi_plus]
+            neighbours += world[wi, hi - 1]
+            neighbours += world[wi, hi_plus]
+            neighbours += world[wi_plus, hi - 1]
+            neighbours += world[wi_plus, hi]
+            neighbours += world[wi_plus, hi_plus]
+            if not world[wi, hi]:
+                if neighbours in born:
+                    next_world[wi, hi] = 1
+            else:
+                if neighbours in survive:
+                    next_world[wi, hi] = 1
+    return next_world
 
-    def next_tick(self, world):
-        """ implements the gol rules: B3,S23 """
-        next_world = numpy.zeros((self.world_width, self.world_height))
-        for wi in range(0, self.world_width):
-            for hi in range(0, self.world_height):
-                wi_plus = wi + 1
-                hi_plus = hi + 1
-                if wi == self.world_width - 1:
-                    wi_plus = -1
-                if hi == self.world_height - 1:
-                    hi_plus = -1
-                neighbours = 0
-                neighbours += world[wi - 1, hi - 1]
-                neighbours += world[wi - 1, hi]
-                neighbours += world[wi - 1, hi_plus]
-                neighbours += world[wi, hi - 1]
-                neighbours += world[wi, hi_plus]
-                neighbours += world[wi_plus, hi - 1]
-                neighbours += world[wi_plus, hi]
-                neighbours += world[wi_plus, hi_plus]
-                if not world[wi, hi]:
-                    # born
-                    if neighbours == 3:
-                        next_world[wi, hi] = 1
-                else:
-                    # survive
-                    if neighbours == 2 or neighbours == 3:
-                        next_world[wi, hi] = 1
-                # print('c=' + str(world[wi, hi]) + '\tn=' + str(neighbours) + '\tt=' + str(next_world[wi, hi]))
-        return next_world
 
-    def show_world(self, world):
-        """ Print the world to screen """
-        for hi in range(0, self.world_height):
-            line = ''
-            for wi in range(0, self.world_width):
-                if world[wi, hi]:
-                    line += 'X'
-                else:
-                    line += '-'
-                line += ''
-            print(line)
-        print('\n\n\n')
+def show_world(world):
+    """ Print the world to screen """
+    (width, height) = world.shape
+    for hi in range(0, height):
+        line = ''
+        for wi in range(0, width):
+            if world[wi, hi]:
+                line += 'X'
+            else:
+                line += '-'
+            line += ''
+        print(line)
--- run_gameoflife.py
+++ run_gameoflife.py
@@ -4,13 +4,34 @@
 """ GOF Main """
 
 import sys
+import time
 from GameOfLife import gameoflife
 
 
 def main(argv):
     """ Main """
-    gof = gameoflife.GameOfLife()
-    gof.run()
+    ticks = 10000
+    sleep = .05
+    world = gameoflife.random_world(width=100, height=50, fill_chance=.05)
+    gameoflife.show_world(world)
+    print('\n')
+    # seen_worlds = dict()
+    # world.flags.writeable = False
+    # seen_worlds[hash(world.data)] = [-1]
+    for tick in range(0, ticks):
+        time.sleep(sleep)
+        world = gameoflife.next_tick(world, born=[3], survive=[2, 3])
+        gameoflife.show_world(world)
+        print('\n')
+        # world.flags.writeable = False
+        # hash_id = hash(world.data)
+        # seen_ticks = [tick]
+        # if hash_id in seen_worlds:
+        #     seen_ticks += seen_worlds[hash_id]
+        #     print('Tick ' + str(tick) + ' loops back to tick ' + str(seen_ticks))
+        # seen_worlds[hash(world.data)] = seen_ticks
+    print("finished")
+
 
 if __name__ == "__main__":
     main(sys.argv)
